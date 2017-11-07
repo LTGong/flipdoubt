b6a0f31da42e9ee126f1417fc017c388556e9d42
Karan Jaswani <karanjaswani123@gmail.com>
2017-11-06 23:14:16 -0600

    basic gallery implementation

--- web/src/FrontPage/index.js
+++ web/src/FrontPage/index.js
@@ -1,11 +1,9 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 //import frustratedMonkey from './frustrated-monkey.gif';
 import './frontpage.css';
 
-// const countStyle = {
-//   color: 'brown',
-// };
+// const countStyle = {   color: 'brown', };
 
 class Frontpage extends Component {
 
@@ -15,77 +13,118 @@ class Frontpage extends Component {
       clickCount: 0,
       value: ''
     }
-    this.foo = this.foo.bind(this);
-    this.transform = this.transform.bind(this);
+    this.foo = this
+      .foo
+      .bind(this);
+    this.transform = this
+      .transform
+      .bind(this);
   }
 
   foo(e) {
-    // this.setState(prevState => {
-    //   return {clickCount: prevState.clickCount + 1}
+    // this.setState(prevState => {   return {clickCount: prevState.clickCount + 1}
     // })
-    this.setState({
-       value: e.target.value
-    })
+    this.setState({value: e.target.value})
     console.log(this.state.value)
   }
 
-  transform(){
+  transform() {
     console.log('we transformin yo');
     fetch('/api/mturk/transform', {
       method: 'POST',
       body: JSON.stringify({thoughtText: this.state.value})
-    })
-    .then((res) => res.json() )
-    .then( (data) => {
+    }).then((res) => res.json()).then((data) => {
       console.log(data)
-    })
-    .catch(
-      err => console.log(err)
-    )
+    }).catch(err => console.log(err))
   }
 
   render() {
     return (
       <div className="FrontPage">
 
-      <div className="box">
-        <div className="field is-grouped">
-          <p className="control is-expanded">
-            <input value={this.state.value} onChange={this.foo} className="input" type="text" placeholder="Purge your thought." />
-          </p>
+        {/* Move this to search component */}
+        <div className="box">
 
-          <p className="control">
-            <a className="button is-info" onClick={this.transform} >
-              Transform
-            </a>
-          </p>
+          <div className="field is-grouped">
+            <p className="control is-expanded">
+              <input
+                value={this.state.value}
+                onChange={this.foo}
+                className="input"
+                type="text"
+                placeholder="Purge your thought."/>
+            </p>
+
+            <p className="control">
+              <a className="button is-info" onClick={this.transform}>
+                Transform
+              </a>
+            </p>
+          </div>
         </div>
-      </div>
 
+        {/* Move this to gallery component */}
+        <div className="box gallery">
+          <h1 className="has-text-centered">Gallery of the Mind</h1>
+          <div className="columns">
+
+            <div className="column is-4">
+            <div className="card">
+                <div className="card-image">
+                  <figure className="image is-4by3">
+                    <img
+                      src="http://www.planwallpaper.com/static/images/beautiful-scenery-wallpaper.jpg"
+                      alt=""/>
+                  </figure>
+                </div>
+                <br/>
+
+                <footer className="card-footer">
+                  <a className="card-footer-item">Tweet on Twitter</a>
+                  <a className="card-footer-item">Post on Facebook</a>
+                </footer>
+              </div>
+            </div>
+
+            <div className="column is-4">
+              <div className="card">
+                <div className="card-image">
+                  <figure className="image is-4by3">
+                    <img
+                      src="http://www.planwallpaper.com/static/images/beautiful-scenery-wallpaper.jpg"
+                      alt=""/>
+                  </figure>
+                </div>
+                <br/>
+
+                <footer className="card-footer">
+                  <a className="card-footer-item">Tweet on Twitter</a>
+                  <a className="card-footer-item">Post on Facebook</a>
+                </footer>
+              </div>
+            </div>
+
+            <div className="column is-4">
+              <div className="card">
+                <div className="card-image">
+                  <figure className="image is-4by3">
+                    <img
+                      src="http://www.planwallpaper.com/static/images/beautiful-scenery-wallpaper.jpg"
+                      alt=""/>
+                  </figure>
+                </div>
+                <br/>
+                <footer className="card-footer">
+                  <a className="card-footer-item">Tweet on Twitter</a>
+                  <a className="card-footer-item">Post on Facebook</a>
+                </footer>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
-
-// <div>
-//   <img src={frustratedMonkey} alt="animated gif of a monkey shoving a laptop off the table" />
-// </div>
-
-// <h1>
-//   <span className="icon"><i className="fa fa-home"></i></span>
-//   &nbsp;
-//   the front page!
-// </h1>
-//
-// <div className="level">
-//   <div className="level-left">
-//     <div className="level-item">
-//       <button className="button" onClick={this.foo}>example button</button>
-//     </div>
-//     <div className="level-item" style={countStyle}>
-//       click count: {this.state.clickCount}
-//     </div>
-//   </div>
-// </div>
 
 export default Frontpage;
--- web/src/Header/index.js
+++ web/src/Header/index.js
@@ -33,17 +33,6 @@ class Header extends Component {
       </div>
     </div>
     </header>
-
-      // <nav className="navbar is-light">
-      //   <div className="navbar-menu">
-      //     <div className="navbar-start">
-      //       <a className="navbar-item title" href="/">5117 React Project Template</a>
-      //     </div>
-
-      //     <LoginLogout {...this.props} />
-      //   </div>
-      // </nav>
-      
     )
   }
   
