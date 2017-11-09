49071eb6f30a8f80e3e6818bcb4914d96001251d
Karan Jaswani <karanjaswani123@gmail.com>
2017-11-09 15:46:29 -0600

    gallery polish

--- server/routes/db-api.js
+++ server/routes/db-api.js
@@ -6,11 +6,10 @@ var _ = require('lodash');
 const checkJwt = require('../auth').checkJwt;
 const fetch = require('node-fetch');
 
-// simple API call, no authentication or user info
-// NOTE FROM ESTELLE: ALL WE SHOULD HAVE TO DO IS ADD THE checkJwt token to
-// this function and change the route name to unprotected when we figure out
-// the authentication issue...
-router.post('/unprotected', function(req, res, next) {
+// simple API call, no authentication or user info NOTE FROM ESTELLE: ALL WE
+// SHOULD HAVE TO DO IS ADD THE checkJwt token to this function and change the
+// route name to unprotected when we figure out the authentication issue...
+router.post('/unprotected', function (req, res, next) {
 
   console.log(req.body);
   // This will come from req.user when we figure out authentication
@@ -18,15 +17,34 @@ router.post('/unprotected', function(req, res, next) {
 
   let newThought = new Thought(req.body.text, null, user_id, req.body.processing, req.body.HITId, req.body.HITTypeId);
 
-  req.db.collection('thoughts').insertOne(newThought)
-      .then(function(result) {
-          console.log('In unprotected db callback');
-          console.log(result.ops[0]);
-          res.status(200).send(result.ops[0]);
-      })
-      .catch(function(error) {
-        throw (error);
-      });
+  req
+    .db
+    .collection('thoughts')
+    .insertOne(newThought)
+    .then(function (result) {
+      console.log('In unprotected db callback');
+      console.log(result.ops[0]);
+      res
+        .status(200)
+        .send(result.ops[0]);
+    })
+    .catch(function (error) {
+      throw(error);
+    });
+});
+
+router.get('/get-user-quotes', function (req, res, next) {
+  // This will come from req.user when we figure out authentication
+  let user_id = 'Temp_Fake_UserID_12345';
+  debugger
+  req
+    .db
+    .collection('thoughts')
+    .find({"_user_id": user_id, "_processing": false})
+    .toArray(function(err, results) {
+      res
+      .json(results);
+    })
 });
 
 router.get('/get-processing-HITs', function(req, res, next) {
@@ -59,34 +77,34 @@ router.post('/update-processed-HIT', function(req, res, next) {
 
 
 // checkJwt middleware will enforce valid authorization token
-router.get('/protected', checkJwt, function(req, res, next) {
+router.get('/protected', checkJwt, function (req, res, next) {
   console.log('In Protected db call');
 
   console.log(req.user);
 
-  req.db.collection('thoughts').find().toArray(function(err, results) {
-    if (err) {
-      next(err);
-    }
-    console.log('In mongo call.')
-    console.log(results);
+  req
+    .db
+    .collection('thoughts')
+    .find()
+    .toArray(function (err, results) {
+      if (err) {
+        next(err);
+      }
+      console.log('In mongo call.')
+      console.log(results);
 
-    res.status(200).send('Eureka!');
-    // res.json({
-    //   db_id: 'Ad ID shall be sent!'
-    // });
-  });
+      res
+        .status(200)
+        .send('Eureka!');
+      // res.json({   db_id: 'Ad ID shall be sent!' });
+    });
   // // the auth0 user identifier for connecting users with data
   // console.log('auth0 user id:', req.user.sub);
   //
-  // // fetch info about the user (this isn't useful here, just for demo)
-  // const userInfoUrl = req.user.aud[1];
-  // const bearer = req.headers.authorization;
-  // fetch(userInfoUrl, {
-  // 	headers: { 'authorization': bearer },
-  // })
-  //   .then(res => res.json())
-  //   .then(userInfoRes => console.log('user info res', userInfoRes))
+  // // fetch info about the user (this isn't useful here, just for demo) const
+  // userInfoUrl = req.user.aud[1]; const bearer = req.headers.authorization;
+  // fetch(userInfoUrl, { 	headers: { 'authorization': bearer }, })   .then(res =>
+  // res.json())   .then(userInfoRes => console.log('user info res', userInfoRes))
   //   .catch(e => console.error('error fetching userinfo from auth0'));
 });
 
--- web/src/Gallery/index.js
+++ web/src/Gallery/index.js
@@ -1,82 +1,132 @@
 import React, {Component} from 'react';
 import './gallery.css';
+import background1 from './background-1.jpg';
+import background2 from './background-2.jpg';
+import background3 from './background-3.jpg';
+import background4 from './background-4.jpg';
+import background5 from './background-5.jpg';
+import background6 from './background-6.jpg';
+import background7 from './background-7.jpg';
+import background8 from './background-8.jpg';
+import background9 from './background-9.jpg';
+import background10 from './background-10.jpg';
+import background11 from './background-11.jpg';
 
 class Gallery extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            thoughts: [],
+        }
+        this.handleCardClick = this
+            .handleCardClick
+            .bind(this);
+    }
+
+    componentWillMount() {
+        fetch('/api/db/get-user-quotes').then((res) => res.json()).then((res) => {
+            console.log(res);
+            this.setState({thoughts: res});
+        }).catch(err => console.log(err));
+    }
+
+    handleCardClick(e) {
+        e.preventDefault();
+        if (e.currentTarget.className.includes('flipped')) {
+            e.currentTarget.className = "custom-card";
+            e.currentTarget.children[0].className = "front";
+            e.currentTarget.children[1].className = "back is-hidden";
+        } else {
+            e.currentTarget.className = "custom-card flipped";
+            e.currentTarget.children[0].className = "front is-hidden";
+            e.currentTarget.children[1].className = "back";
+        }
+    }
+
+    getBackground(){
+        switch(Math.floor(Math.random() * 10) + 1 ) {
+            case 1:
+                return background1;
+            case 2:
+                return background2;
+            case 3:
+                return background3;
+            case 4:
+                return background4;
+            case 5:
+                return background5;
+            case 6:
+                return background6;
+            case 7:
+                return background7;
+            case 8:
+                return background8;
+            case 9:
+                return background9;
+            case 10:
+                return background10;
+            default:
+                return background1;
+        }
+    }
+
     render() {
+
+        let setsOfThree = [];
+        let i = 0;
+        while(i < this.state.thoughts.length){
+            setsOfThree.push(this.state.thoughts.slice(i, i + 3));
+            i += 3
+        }   
+
+        let templates = [];
+        for(i = 0; i < setsOfThree.length; i++) {
+            templates.push(
+                setsOfThree[i].map((thought, i) => 
+                {   
+                    return (<div className="column is-4">
+                            <div className="card-container">
+                            <div className="custom-card" onClick={this.handleCardClick}>
+                            
+                            <figure className="front">
+                                <img src={this.getBackground()} alt="front"/>
+                                <div className="caption">
+                                    <h2>{thought._pos_thought}</h2>
+                                    <div className="share-social">
+                                        <i className="fa fa-twitter" aria-hidden="true"></i>
+                                        <i className="fa fa-google-plus" aria-hidden="true"></i>
+                                        <i className="fa fa-facebook" aria-hidden="true"></i>
+                                    </div>
+                                </div>
+                            </figure>
+
+                            <figure className="back is-hidden">
+                                <img src={background11} alt="back"/>
+                                <div className="caption">
+                                    <h2>{thought._neg_thought}</h2>
+                                </div>
+                            </figure>
+                            </div>
+                            </div>
+                            </div>
+                        );
+                })
+            );
+        }
+
+        var gallery_template = 
+            templates
+            .map((template, i) => 
+                <div className="columns">
+                    {template}
+                </div>
+        );
         return (
             <div className="box">
                 <h2 className="title is-3 has-text-centered">Gallery of the Mind</h2>
                 <br/>
-                <div className="columns">
-                    <div className="column is-4">
-                        <div className="card-container">
-                            <div className="custom-card">
-                                <figure className="front">
-                                    <img src="http://www.jboeijenga.nl/img/front.jpg" alt="front"/>
-                                    <div className="caption">
-                                        <h2>be fearless in the pursuit of what sets your soul on fire</h2>
-                                        <i className="fa fa-twitter" aria-hidden="true"></i>
-                                        <i className="fa fa-google-plus" aria-hidden="true"></i>
-                                        <i className="fa fa-facebook" aria-hidden="true"></i>
-                                    </div>
-                                </figure>
-
-                                <figure className="back is-hidden">
-                                    <img src="http://www.jboeijenga.nl/img/back.jpg" alt="back"/>
-                                    <div className="caption">
-                                        <h2>I want to learn photography but so scared of how the photographs would turn out.</h2>
-                                    </div>
-                                </figure>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="column is-4">
-                        <div className="card-container">
-                            <div className="custom-card">
-                                <figure className="front">
-                                    <img src="http://www.jboeijenga.nl/img/front.jpg" alt="front"/>
-                                    <div className="caption">
-                                        <h2>be fearless in the pursuit of what sets your soul on fire</h2>
-                                        <i className="fa fa-twitter" aria-hidden="true"></i>
-                                        <i className="fa fa-google-plus" aria-hidden="true"></i>
-                                        <i className="fa fa-facebook" aria-hidden="true"></i>
-                                    </div>
-                                </figure>
-
-                                <figure className="back is-hidden">
-                                    <img src="http://www.jboeijenga.nl/img/back.jpg" alt="back"/>
-                                    <div className="caption">
-                                        <h2>I want to learn photography but so scared of how the photographs would turn out.</h2>
-                                    </div>
-                                </figure>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="column is-4">
-                        <div className="card-container">
-                            <div className="custom-card">
-                                <figure className="front">
-                                    <img src="http://www.jboeijenga.nl/img/front.jpg" alt="front"/>
-                                    <div className="caption">
-                                        <h2>be fearless in the pursuit of what sets your soul on fire</h2>
-                                        <i className="fa fa-twitter" aria-hidden="true"></i>
-                                        <i className="fa fa-google-plus" aria-hidden="true"></i>
-                                        <i className="fa fa-facebook" aria-hidden="true"></i>
-                                    </div>
-                                </figure>
-
-                                <figure className="back is-hidden">
-                                    <img src="http://www.jboeijenga.nl/img/back.jpg" alt="back"/>
-                                    <div className="caption">
-                                        <h2>I want to learn photography but so scared of how the photographs would turn out.</h2>
-                                    </div>
-                                </figure>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                {gallery_template}
             </div>
         );
     }
